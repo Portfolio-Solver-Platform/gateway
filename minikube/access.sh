a104f0f14d5ce7ca7f14548fdf39d6aee1721388
Jonas Bork <jonas10bork@gmail.com>
2025-09-09 10:21:00 +0200

    Improved access script

--- minikube/access.sh
+++ minikube/access.sh
@@ -6,22 +6,21 @@ SERVICE="gateway-nginx"
 EXTERNAL_IP=$(kubectl get svc "$SERVICE" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 if [ -z "$EXTERNAL_IP" ]; then
-  echo "❌ No external IP found for $SERVICE."
+  echo "❌ No external IP found for gateway."
   echo "👉 Starting minikube tunnel (requires sudo)..."
 
   # Start minikube tunnel in the background
   minikube tunnel >/dev/null 2>&1 &
-  sleep 1s
 
   echo "⏳ Waiting for external IP..."
   while true; do
     EXTERNAL_IP=$(kubectl get svc "$SERVICE" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
     if [ -n "$EXTERNAL_IP" ]; then
-      echo "✅ External IP for $SERVICE: $EXTERNAL_IP"
+      echo "✅ External IP for gateway: $EXTERNAL_IP"
       break
     fi
     sleep 2
   done
 else
-  echo "✅ External IP for $SERVICE: $EXTERNAL_IP"
+  echo "✅ External IP for gateway: $EXTERNAL_IP"
 fi
